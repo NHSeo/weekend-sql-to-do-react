2129a7a2fe484aa52f1dac7064e31108b7473434
Noah Seo <sht6205@gmail.com>
2024-05-27 23:35:29 -0500

    Updated POST router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -17,7 +17,23 @@ router.get('/', (req, res) => {
   });
 
 // POST
+router.post('/', (req, res) => {
+    console.log('request.body', req.body);
 
+    let newTask = req.body;
+    let title = newTask.title;
+
+    let queryText = `INSERT INTO tasks (title, completed) VALUES ($1, $2);`;
+    pool.query(queryText, [title, false])
+        .then(dbResult => {
+            console.log('dbResult.rows', dbResult.rows);
+            res.sendStatus(201);
+        })
+        .catch(dbError => {
+            console.log('POST Error:', dbError);
+            res.sendStatus(500);
+        });
+});
 
 // PUT
 
