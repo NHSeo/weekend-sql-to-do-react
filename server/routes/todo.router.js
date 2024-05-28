96efd1b8097f7972f16e1c6070b61928e074eee5
Noah Seo <sht6205@gmail.com>
2024-05-27 23:30:03 -0500

    Updated GET router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,10 +3,24 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    const queryText = 'SELECT * FROM "tasks";';
+    pool.query(queryText)
+      .then((dbResult) => {
+        let toDoList = dbResult.rows;
+        res.send(toDoList);
+      })
+      .catch((dbError) => {
+        console.error('GET Error', dbError);
+        res.sendStatus(500);
+      });
+  });
 
 // POST
 
+
 // PUT
+
 
 // DELETE
 
